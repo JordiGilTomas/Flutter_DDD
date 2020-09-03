0065b936aeaca892eb6d6d0bdf84e47b4d0587ce
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-09-03 12:46:48 +0200

    Firebase Auth Setup & Facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:fluter_ddd/domain/core/errors.dart';
 import 'package:fluter_ddd/domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,6 +8,13 @@ abstract class ValueObject<T> {
   const ValueObject();
 
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    // id helper comes from Dartz
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight();
 
